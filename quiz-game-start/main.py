e3396fade21e063d5f1f3dd4cba4b0365a877203
Katlego <kdhlamini023b@student.wethinkcode.co.za>
2024-08-04 02:19:40 +0200

    next question added

--- quiz-game-start/main.py
+++ quiz-game-start/main.py
@@ -1,2 +1,16 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank=[]
+for i in question_data:
+    # question_bank  = Question()
+    text = i["text"]
+    answer = i["answer"]
+    new_question=  Question(text,answer)
+    question_bank.append(new_question)
+
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
+
